95c572951ed810ef700cd4d67f5aa155191864e5
DeusExMachinaFTC <ex.machina.dem@gmail.com>
2022-12-02 19:08:49 -0600

    update

--- wirelessrc.ps1
+++ wirelessrc.ps1
@@ -3,8 +3,16 @@ while($confirmation -ne "ok")
 {
     $confirmation = Read-Host "Plug in the phone [ok]"
 }
-adb tcpip 5555
+adb tcpip 20180
 $IP = Read-Host -Prompt 'Disconnect phone and input IP address from phone'
-$PORT = $IP+":5555"
+$PORT = $IP+":20180"
 adb connect $PORT
+scrcpy --always-on-top
+Write-Host "You can now minimize this window and come back to disconnect the phone"
+$confirmation2 = Read-Host "Disconnect? [ok]"
+while($confirmation2 -ne "ok")
+{
+    $confirmation2 = Read-Host "Disconnect? [ok]"
+}
+adb disconnect $PORT
 pause
