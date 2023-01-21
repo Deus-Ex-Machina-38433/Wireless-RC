207ec2ad803475319a949579da3e442c109c69c3
Rishu Sharma <83421723+Firebolt9907@users.noreply.github.com>
2023-01-20 22:14:57 -0600

    final commit

--- wirelessrc.ps1
+++ wirelessrc.ps1
@@ -7,7 +7,6 @@ adb tcpip 20180
 $IP = Read-Host -Prompt 'Disconnect phone and input IP address from phone'
 $PORT = $IP+":20180"
 adb connect $PORT
-scrcpy --always-on-top
 Write-Host "You can now minimize this window and come back to disconnect the phone"
 $confirmation2 = Read-Host "Disconnect? [ok]"
 while($confirmation2 -ne "ok")
